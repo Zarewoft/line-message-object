9e3b5f4e67b6c753250cfd82fa2a605ae7eca25f
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2018-06-19 14:24:08 +0700

    Add jsdoc on sticker message

--- src/message_objects/sticker_message/sticker_message.ts
+++ src/message_objects/sticker_message/sticker_message.ts
@@ -1,13 +1,17 @@
 import { MessageObject } from "../message_object";
 
-/**
- * StickerMessage
- */
 class StickerMessage extends MessageObject {
   type: string
   packageId: string
   stickerId: string
 
+  /**
+   * Create line message object type sticker
+   *
+   * ref: https://developers.line.me/en/docs/messaging-api/reference/#sticker-message
+   * @param {string} packageId
+   * @param {string} stickerId
+   */
   constructor(packageId: string = "", stickerId: string = "") {
     super();
     this.packageId = packageId;
