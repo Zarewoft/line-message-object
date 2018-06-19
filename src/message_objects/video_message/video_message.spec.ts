86204cb169bf92ce2c77969dd9a74432572cf524
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2018-06-19 18:18:29 +0700

    Implement new constructor with validate field

--- src/message_objects/video_message/video_message.spec.ts
+++ src/message_objects/video_message/video_message.spec.ts
@@ -2,28 +2,41 @@ import { assert } from "chai";
 import { VideoMessage } from "./video_message";
 
 describe("VideoMessage", () => {
-  it("should construct type property 'video'", () => {
-    const videoMessage = new VideoMessage();
-    assert.equal(videoMessage.getJSON().type, "video");
+  it("should construct video message 1", () => {
+    const videoMessage = VideoMessage.New("https://content.mp4", "https://image.jpg")
+    assert.deepEqual(videoMessage.message.getJSON(), {
+      type: "video",
+      originalContentUrl: "https://content.mp4",
+      previewImageUrl: "https://image.jpg"
+    });
   });
 
-  it("should construct originalContentUrl property", () => {
-    const videoMessage = new VideoMessage("originalContentUrl");
-    assert.equal(videoMessage.getJSON().originalContentUrl, "originalContentUrl");
+  it("should construct video message 2", () => {
+    const videoMessage = VideoMessage.New("https://content2.mp4", "https://image2.jpg")
+    assert.deepEqual(videoMessage.message.getJSON(), {
+      type: "video",
+      originalContentUrl: "https://content2.mp4",
+      previewImageUrl: "https://image2.jpg"
+    });
   });
 
-  it("should construct previewImageUrl property", () => {
-    const videoMessage = new VideoMessage(null, "previewImageUrl");
-    assert.equal(videoMessage.getJSON().previewImageUrl, "previewImageUrl");
+  it("should return error when content url is undefined or empty", () => {
+    const videoMessage = VideoMessage.New("", "https://image.jpg")
+    assert.equal(videoMessage.error.message, "original content url is missing")
   });
 
-  it("should construct default value with empty string for originalContentUrl property", () => {
-    const videoMessage = new VideoMessage();
-    assert.equal(videoMessage.getJSON().originalContentUrl, "");
+  it("should return error when preview image url is undefined or empty", () => {
+    const videoMessage = VideoMessage.New("https://content.mp4", "")
+    assert.equal(videoMessage.error.message, "preview image url is missing");
   });
 
-  it("should construct default value with empty string for previewImageUrl property", () => {
-    const videoMessage = new VideoMessage();
-    assert.equal(videoMessage.getJSON().previewImageUrl, "");
+  it("should return error when content url is not https", () => {
+    const videoMessage = VideoMessage.New("http://content2.mp4", "https://image.jpg")
+    assert.equal(videoMessage.error.message, "original content url is require https protocol")
+  });
+
+  it("should return error when preview image url is not https", () => {
+    const videoMessage = VideoMessage.New("https://content2.mp4", "http://image.jpg")
+    assert.equal(videoMessage.error.message, "preview image url is require https protocol")
   });
 });
--- src/message_objects/video_message/video_message.ts
+++ src/message_objects/video_message/video_message.ts
@@ -1,4 +1,4 @@
-import { MessageObject } from "../message_object";
+import { MessageObject, MessageConstructResult } from "../message_object";
 
 type VideoJSON = {
   type: string,
@@ -18,13 +18,43 @@ class VideoMessage extends MessageObject {
    * @param {string} originalContentUrl
    * @param {string} previewImageUrl
    */
-  constructor(originalContentUrl: string = "", previewImageUrl: string = "") {
+  constructor(originalContentUrl: string, previewImageUrl: string) {
     super();
     this._type = "video";
     this._originalContentUrl = originalContentUrl;
     this._previewImageUrl = previewImageUrl;
   }
 
+  /**
+   * Create line message object type video
+   *
+   * ref: https://developers.line.me/en/docs/messaging-api/reference/#video-message
+   * @param {string} originalContentUrl
+   * @param {string} previewImageUrl
+   * @returns {MessageConstructResult<VideoMessage>}
+   */
+  static New(originalContentUrl: string, previewImageUrl: string): MessageConstructResult<VideoMessage> {
+    if (!originalContentUrl) {
+      return { message: null, error: new Error("original content url is missing") };
+    } else if (!originalContentUrl.startsWith("https://", 0)) {
+      return { message: null, error: new Error("original content url is require https protocol") };
+    }
+
+    if (!previewImageUrl) {
+      return { message: null, error: new Error("preview image url is missing") };
+    } else if (!previewImageUrl.startsWith("https://", 0)) {
+      return { message: null, error: new Error("preview image url is require https protocol") };
+    }
+
+    const videoMessage = new VideoMessage(originalContentUrl, previewImageUrl);
+    return { message: videoMessage, error: null };
+  }
+
+  /**
+   * Return line video message object
+   *
+   * @returns {VideoJSON}
+   */
   getJSON(): VideoJSON {
     return {
       type: this._type,
