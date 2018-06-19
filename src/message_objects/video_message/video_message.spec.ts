e74b4185601c8dedbb3f047e7f4d2cf686f530f6
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2018-06-19 14:34:55 +0700

    Implement line video message

---
+++ src/message_objects/video_message/video_message.spec.ts
@@ -0,0 +1,29 @@
+import { assert } from "chai";
+import { VideoMessage } from "./video_message";
+
+describe("VideoMessage", () => {
+  it("should construct type property 'video'", () => {
+    const videoMessage = new VideoMessage();
+    assert.equal(videoMessage.type, "video");
+  });
+
+  it("should construct originalContentUrl property", () => {
+    const videoMessage = new VideoMessage("originalContentUrl");
+    assert.equal(videoMessage.originalContentUrl, "originalContentUrl");
+  });
+
+  it("should construct previewImageUrl property", () => {
+    const videoMessage = new VideoMessage(null, "previewImageUrl");
+    assert.equal(videoMessage.previewImageUrl, "previewImageUrl");
+  });
+
+  it("should construct default value with empty string for originalContentUrl property", () => {
+    const videoMessage = new VideoMessage();
+    assert.equal(videoMessage.originalContentUrl, "");
+  })
+
+  it("should construct default value with empty string for previewImageUrl property", () => {
+    const videoMessage = new VideoMessage();
+    assert.equal(videoMessage.previewImageUrl, "");
+  })
+});
---
+++ src/message_objects/video_message/video_message.ts
@@ -0,0 +1,23 @@
+import { MessageObject } from "../message_object"
+
+class VideoMessage extends MessageObject {
+  type: string
+  originalContentUrl: string
+  previewImageUrl: string
+
+  /**
+   * Create line message object type video
+   *
+   * ref: https://developers.line.me/en/docs/messaging-api/reference/#video-message
+   * @param {string} originalContentUrl
+   * @param {string} previewImageUrl
+   */
+  constructor(originalContentUrl: string = "", previewImageUrl: string = "") {
+    super();
+    this.type = "video";
+    this.originalContentUrl = originalContentUrl;
+    this.previewImageUrl = previewImageUrl;
+  }
+}
+
+export { VideoMessage };
