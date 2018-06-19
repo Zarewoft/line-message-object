4e885e176de8319005bd261b3bf9ac3aff7ee477
Tanakrit Saisillapee <tanakrit.s@hbot.io>
2018-06-19 18:16:22 +0700

    Implement construct message

--- src/message_objects/image_message/image_message.spec.ts
+++ src/message_objects/image_message/image_message.spec.ts
@@ -2,8 +2,21 @@ import { ImageMessage } from "./image_message";
 import { assert } from "chai";
 
 describe("ImageMessage", () => {
-    it("should construct with type 'image'", () => {
-        const imageMessage = new ImageMessage();
-        assert.equal(imageMessage.getJSON().type, "image");
+    it("should construct with message 1", () => {
+        const imageMessage = ImageMessage.New("https://www.img.com/img.jpg", "https://www.img.com/preview.jpg")
+        assert.deepEqual(imageMessage.message.getJSON(), {
+          type: "image",
+          originalContentUrl: "https://www.img.com/img.jpg",
+          previewImageUrl: "https://www.img.com/preview.jpg"
+        });
     });
+
+    it("should construct with message 2", () => {
+      const imageMessage = ImageMessage.New("https://www.img2.com/img.jpg", "https://www.img2.com/preview.jpg")
+      assert.deepEqual(imageMessage.message.getJSON(), {
+        type: "image",
+        originalContentUrl: "https://www.img2.com/img.jpg",
+        previewImageUrl: "https://www.img2.com/preview.jpg"
+      });
+  });
 });
--- src/message_objects/image_message/image_message.ts
+++ src/message_objects/image_message/image_message.ts
@@ -1,19 +1,25 @@
 import { MessageObject, MessageConstructResult } from "../message_object";
 
 type ImageJSON = {
-  type: string
+  type: string,
+  originalContentUrl: string,
+  previewImageUrl: string
 }
 
 class ImageMessage extends MessageObject {
   private _type: string;
+  private _originalContentUrl: string;
+  private _previewImageUrl: string;
 
-  constructor() {
+  constructor(originalContentUrl: string, previewImageUrl: string) {
     super();
     this._type = "image";
+    this._originalContentUrl = originalContentUrl;
+    this._previewImageUrl = previewImageUrl;
   }
 
-  static New(originalContentUrl: string): MessageConstructResult<ImageMessage> {
-    const imageMessage = new ImageMessage();
+  static New(originalContentUrl: string, previewImageUrl: string): MessageConstructResult<ImageMessage> {
+    const imageMessage = new ImageMessage(originalContentUrl, previewImageUrl);
     return {
       message: imageMessage,
       error: null
@@ -22,7 +28,9 @@ class ImageMessage extends MessageObject {
 
   getJSON(): ImageJSON {
     return {
-      type: this._type
+      type: this._type,
+      originalContentUrl: this._originalContentUrl,
+      previewImageUrl: this._previewImageUrl
     };
   }
 }
