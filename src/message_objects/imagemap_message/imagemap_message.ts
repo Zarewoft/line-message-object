d61c430cce8e2f7b004124c7d0343687275ea6db
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2018-06-21 18:10:08 +0700

    Reimplement imagemap message

--- src/message_objects/imagemap_message/imagemap_message.ts
+++ src/message_objects/imagemap_message/imagemap_message.ts
@@ -1,47 +1,44 @@
-import { MessageObject } from "../message_object";
-
-type ImagemapArea = {
-  x: number,
-  y: number,
-  width: number,
-  height: number
-};
-
-type ImagemapAction = {
-  type: string,
-  label?: string,
-  linkUri: string,
-  area: ImagemapArea
-};
-
-type ImagemapBaseSize = {
-  width: number,
-  height: number
-};
+import { MessageObject, MessageConstructResult } from "../message_object";
+import { BaseSize, BaseSizeJSON } from "../../base_size";
+import { ImagemapURIAction, ImagemapURIActionJSON } from "../../action_objects/imagemap_action/imagemap_uri_action";
+import { ImagemapMessageAction, ImagemapMessageActionJSON } from "../../action_objects/imagemap_action/imagemap_message_action";
 
 type ImagemapJSON = {
   type: string,
   baseUrl: string,
   altText: string,
-  baseSize: ImagemapBaseSize,
-  actions: ImagemapAction[]
+  baseSize: BaseSizeJSON,
+  actions: (ImagemapURIActionJSON | ImagemapMessageActionJSON)[],
 };
 
 class ImagemapMessage extends MessageObject {
   private _type: string;
   private _baseUrl: string;
   private _altText: string;
-  private _baseSize: ImagemapBaseSize;
-  private _actions: ImagemapAction[];
+  private _baseSize: BaseSize;
+  private _actions: (ImagemapURIAction | ImagemapMessageAction)[];
 
-  constructor(baseUrl: string, altText: string, baseSize: ImagemapBaseSize, actions: ImagemapAction[]) {
+  private constructor(baseUrl: string, altText: string, baseSize: BaseSize, actions: (ImagemapURIAction | ImagemapMessageAction)[]) {
     super();
     this._type = "imagemap";
     this._baseUrl = baseUrl;
     this._altText = altText;
-    this._baseSize.width = 1040;
-    this._baseSize.height = baseSize.height;
+    this._baseSize = baseSize;
     this._actions = actions;
+  }
+
+  public static New(baseUrl: string, altText: string, baseSize: BaseSize, actions: (ImagemapURIAction | ImagemapMessageAction)[]): MessageConstructResult<ImagemapMessage> {
+    let message: ImagemapMessage;
+    let error: Error;
+
+    if (error = MessageObject.validateURL(baseUrl)) {
+      error.message = `base ${error.message}`;
+      return { message, error };
+    }
+
+    altText = MessageObject.limitText(altText, 400);
+    message = new ImagemapMessage(baseUrl, altText, baseSize, actions);
+    return { message, error };
   }
 
   getJSON(): ImagemapJSON {
@@ -49,8 +46,8 @@ class ImagemapMessage extends MessageObject {
       type: this._type,
       baseUrl: this._baseUrl,
       altText: this._altText,
-      baseSize: this._baseSize,
-      actions: this._actions
+      baseSize: this._baseSize.getJSON(),
+      actions: this._actions.map(action => action.getJSON()),
     };
   }
 }
