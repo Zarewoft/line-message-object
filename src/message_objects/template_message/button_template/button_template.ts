83faef07f32d0324b5fd917b1077f882c2dbf6a9
Tanakrit Saisillapee <tanakrit.s@hbot.io>
2018-06-21 16:38:26 +0700

    Add button template class

---
+++ src/message_objects/template_message/button_template/button_template.ts
@@ -0,0 +1,56 @@
+import { MessageObject } from "../../message_object";
+import { ActionType, ActionJSONType } from "../../../action_objects";
+
+type ButtonTemplateJSON = {
+  type: string,
+  thumbnailImageUrl: string,
+  text: string,
+  actions: ActionJSONType[]
+  imageAspectRatio?: string,
+  imageSize?: string,
+  imageBackgroundColor?: string,
+  title?: string,
+  defaultAction?: ActionJSONType,
+};
+
+class ButtonTemplate extends MessageObject {
+  private _type: string;
+  private _thumbnailImageUrl: string;
+  private _text: string;
+  private _actions: ActionType[];
+  private _imageAspectRatio?: string;
+  private _imageSize?: string;
+  private _imageBackgroundColor?: string;
+  private _title?: string;
+  private _defaultAction?: ActionType;
+
+  constructor(thumbnailImageUrl: string, text: string, actions: ActionType[], imageAspectRatio?: string, imageSize?: string, imageBackgroundColor?: string, title?: string, defaultAction?: ActionType) {
+    super();
+    this._type = "buttons";
+    this._thumbnailImageUrl = thumbnailImageUrl;
+    this._text = text;
+    this._actions = actions;
+    this._imageAspectRatio = imageAspectRatio;
+    this._imageSize = imageSize;
+    this._imageBackgroundColor = imageBackgroundColor;
+    this._title = title;
+    this._defaultAction = defaultAction;
+  }
+
+  getJSON(): ButtonTemplateJSON {
+    const actions = this._actions.map(action => action.getJSON());
+    return {
+      type: this._type,
+      thumbnailImageUrl: this._thumbnailImageUrl,
+      text: this._text,
+      actions: actions,
+      imageAspectRatio: this._imageAspectRatio,
+      imageSize: this._imageSize,
+      imageBackgroundColor: this._imageBackgroundColor,
+      title: this._title,
+      defaultAction: this._defaultAction.getJSON()
+    };
+  }
+}
+
+export { ButtonTemplate };
