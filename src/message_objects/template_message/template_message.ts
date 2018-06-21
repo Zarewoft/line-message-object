9e679f67247fbf9239bf68e57ca3a2dd6c934cb9
Tanakrit Saisillapee <tanakrit.s@hbot.io>
2018-06-21 16:38:36 +0700

    Add template message

---
+++ src/message_objects/template_message/template_message.ts
@@ -0,0 +1,33 @@
+import { MessageObject } from "../message_object";
+import { ButtonTemplate } from "./button_template/button_template";
+
+type TemplateJSON = {
+  type: string,
+  altText: string,
+  template: object
+};
+
+type Template = ButtonTemplate;
+
+class TemplateMessage extends MessageObject {
+  private _type: string;
+  private _altText: string;
+  private _template: Template;
+
+  constructor(altText: string, template: Template) {
+    super();
+    this._type = "template";
+    this._altText = altText;
+    this._template = template;
+  }
+
+  getJSON(): TemplateJSON {
+    return {
+      type: this._type,
+      altText: this._altText,
+      template: this._template.getJSON()
+    };
+  }
+}
+
+export { TemplateMessage };
