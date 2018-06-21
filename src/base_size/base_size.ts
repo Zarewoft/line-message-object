f1e954e8836997bd993ae84ac08f46c6946006e5
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2018-06-21 18:08:41 +0700

    Implement BaseSize class

--- src/base_size/base_size.ts
+++ src/base_size/base_size.ts
@@ -1,6 +1,27 @@
-class BaseSize {
-  width: number;
-  height: number;
+import { MessageObject, MessageConstructResult } from "../message_objects/message_object";
+
+type BaseSizeJSON = { width: number, height: number };
+
+class BaseSize extends MessageObject {
+  private _width: number;
+  private _height: number;
+
+  constructor(height: number) {
+    super();
+    this._width = 1040;
+    this._height = height;
+  }
+
+  public static New(height: number): MessageConstructResult<BaseSize> {
+    return { message: new BaseSize(height), error: null };
+  }
+
+  public getJSON(): BaseSizeJSON {
+    return {
+      width: this._width,
+      height: this._height,
+    };
+  }
 }
 
-export { BaseSize };
+export { BaseSize, BaseSizeJSON };
