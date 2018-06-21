832fecfcd31e410fa5b2a2d651fd541f74e66587
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2018-06-21 18:08:31 +0700

    Implement Area class

--- src/area/area.ts
+++ src/area/area.ts
@@ -1,8 +1,39 @@
-class Area {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
+import { MessageObject, MessageConstructResult } from "../message_objects/message_object";
+
+type AreaJSON = {
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+};
+
+class Area extends MessageObject {
+  private _x: number;
+  private _y: number;
+  private _width: number;
+  private _height: number;
+
+  constructor(x: number, y: number, width: number, height: number) {
+    super();
+    this._x = x;
+    this._y = y;
+    this._width = width;
+    this._height = height;
+  }
+
+  public static New(x: number, y: number, width: number, height: number): MessageConstructResult<Area> {
+    const area = new Area(x, y, width, height);
+    return { message: area, error: null };
+  }
+
+  public getJSON(): AreaJSON {
+    return {
+      x: this._x,
+      y: this._y,
+      width: this._width,
+      height: this._height,
+    };
+  }
 }
 
-export { Area };
+export { Area, AreaJSON };
