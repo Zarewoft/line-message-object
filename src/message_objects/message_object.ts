39e7cda24b8537d29244c6e3960f2494cdeadf71
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2018-06-21 18:09:18 +0700

    Fix limit text is undefined

--- src/message_objects/message_object.ts
+++ src/message_objects/message_object.ts
@@ -11,7 +11,7 @@ abstract class MessageObject {
    * @returns {string}
    */
   static limitText(text: string, maxCharacters: number) {
-    if (text.length > maxCharacters) {
+    if (text && text.length > maxCharacters) {
       return text.slice(0, maxCharacters);
     }
     return text;
